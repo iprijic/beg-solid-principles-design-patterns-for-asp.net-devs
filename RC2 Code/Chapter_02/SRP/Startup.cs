81785af8f160a51b4f762fd833b9ece0f98f55e3
iprijic <iprijic21@gmail.com>
2024-04-09 22:17:32 +0200

    Add project files.

---
+++ RC2 Code/Chapter_02/SRP/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+using SRP.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace SRP
+{
+    public class Startup
+    {
+
+        public Startup(IWebHostEnvironment env)
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath);
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            AppSettings.ConnectionString = config["Data:DefaultConnection:ConnectionString"];
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc(opt => opt.EnableEndpointRouting = false);
+            services.AddControllersWithViews();
+
+
+            services.AddEntityFrameworkSqlServer();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //app.UseMvc(routes =>
+            app.UseEndpoints(routes =>
+            {
+            // routes.MapRoute(
+            routes.MapControllerRoute(
+                 name: "default",
+                    //template: "{controller=Home}/{action=Index}/{id?}");
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Apress Source Code
+
+This repository accompanies [*Beginning SOLID Principles and Design Patterns for ASP.NET  Developers*](http://www.apress.com/9781484218471) by Bipin Joshi (Apress, 2016).
+
+![Cover image](9781484218471.jpg)
+
+Download the files as a zip using the green button, or clone the repository to your machine using Git.
+
+## Updating to ASP.NET Core 1.0
+
+If you have been reading this book, then you might have noticed that it uses RC1 of ASP.NET Core. After the book got published I updated the book's source code to RC2. Now that ASP.NET Core 1.0 is already released you might want to migrate the source code from RC2 to 1.0. To that end [this](http://www.binaryintellect.net/articles/458757c8-6a7a-4a3b-8ab2-e630f6d58585.aspx) article provides an outline of the steps involved in upgrading the book's source code from ASP.NET Core RC2 to ASP.NET Core 1.0.
+
+## Releases
+
+Release v1.0 corresponds to the code in the published book, without corrections or updates.
+
+## Contributions
+
+See the file Contributing.md for more information on how you can contribute to this repository.
